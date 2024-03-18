dbe8951869cfa6d1d47cae58f327a297e1b6d214
Sergey Beskorovainy <bs261257@gmail.com>
2024-03-18 08:03:40 +0000

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<nav>
+  <a href="/">Home</a>
+  <a href="/about">About</a>
+  <a href="/settings">Settings</a>
+  <a href="/settings/profile">Profile</a>
+  <a href="/settings/notifications">Notifications</a>
+</nav>
+
+<slot></slot>
---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import type { PageData } from './$types';
+
+  export let data: PageData;
+</script>
+
+<h1>{data.title}</h1>
+<div>{@html data.content}</div>
+
+<!-- Back to blogs -->
+<a href="/blog">Back to blog</a>
