09f2863c572c5895401801f2e82a235beaa1e509
Sergey Beskorovainy <bs261257@gmail.com>
2024-03-28 07:45:36 +0000

    Update

--- src/routes/api/+page.svelte
+++ src/routes/api/+page.svelte
@@ -3,9 +3,9 @@
 <div class="submenu">
   <p><b>Example of working API with the server (calculating values)</b></p>  
   <ul>
-    <li><a href="/api/add-form">Get the sum of two digits (HTTP method: POST for form)</a></li>
-    <li><a href="/api/add-form_actions">Get the sum of two digits (HTTP method: POST for form actions)</a></li>
-    <li><a href="/api/calc-form_actions">Get the result of the calculation (HTTP method: POST for form actions)</a></li>
+    <li><a href="/api/add-form">01. Get the sum of two digits (HTTP method: POST for form)</a></li>
+    <li><a href="/api/add-form_actions">02. Get the sum of two digits (HTTP method: POST for form actions)</a></li>
+    <li><a href="/api/calc-form_actions">03. Get the result of the calculation (HTTP method: POST for form actions)</a></li>
   </ul>
 
   <p><b>Example of working API with the <a href="https://api.openbrewerydb.org">OpenBreweryDB</a> (public API for brewery-related data)</b></p>  
@@ -16,11 +16,10 @@
     <li><a href="/api/fake-data/openbrewerydb/random">04. Get a random brewery (HTTP method: GET for server)</a></li>
     <li><a href="/api/fake-data/openbrewerydb/random/size/3">05. Number of breweries to return each call (HTTP method: GET for server)</a></li>
     <li><a href="/api/fake-data/openbrewerydb/search/query/san%20diego/per_page/3">06. Search for breweries based on a search term (HTTP method: GET for server)</a></li>
-
-    <li><a href="/api/openbrewerydb/autocomplete?query=san%20diego">Return a list of names and ids of breweries based on a search term (HTTP method: GET for server)</a></li>
-    <li><a href="/api/openbrewerydb/meta">Show all breweries meta data (HTTP method: GET for server)</a></li>
-    <li><a href="/api/openbrewerydb/meta?by_country=south_korea">Show South Korean breweries meta data (HTTP method: GET for server)</a></li>
-    <li><a href="/api/openbrewerydb/meta?by_type=micro">Show micro breweries meta data (HTTP method: GET for server)</a></li>
+    <li><a href="/api/fake-data/openbrewerydb/autocomplete/query/san%20diego">07. Return a list of names and ids of breweries based on a search term (HTTP method: GET for server)</a></li>
+    <li><a href="/api/fake-data/openbrewerydb/meta">08. Show all breweries meta data (HTTP method: GET for server)</a></li>
+    <li><a href="/api/fake-data/openbrewerydb/meta/by_country/south_korea">09. Show South Korean breweries meta data (HTTP method: GET for server)</a></li>
+    <li><a href="/api/fake-data/openbrewerydb/meta/by_type/micro">10. Show micro breweries meta data (HTTP method: GET for server)</a></li>
   </ul>
 
   <p><b>Example of working API with the server (get fake-data)</b></p>  
--- src/routes/api/fake-data/[name]/[type]/[...path]/+server.ts
+++ src/routes/api/fake-data/[name]/[type]/[...path]/+server.ts
@@ -1,56 +1,20 @@
 import { error } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 import { inspector } from '$lib/sys/util';
-// import { chunk } from 'lodash';//  as loChunk
-import loChunk from 'lodash/chunk';
-import loForEach from 'lodash/forEach';
+import { fakeDataService } from "$lib/service-helpers/fakedata.service";
 
 const isDebug = false;
-
-async function getFakeData(url: URL) {
-
-	const response = await fetch(url);
-	if (response && isDebug) inspector('getBreweryData.response:', response)
-	const responseData = await response.json();
-	if (responseData && isDebug) inspector('getBreweryData.responseData:', responseData);
-
-	return JSON.stringify(responseData);
-}
 
 export const GET: RequestHandler = async (reqEvent: any) => {
 	if (reqEvent && isDebug) inspector('RequestEvent:', reqEvent)
 
-	let url: URL;
-	let search: string = '';
-	let result = null;
+	let result: string | undefined = '';
+	
 	try {
-
-		const serviceName = reqEvent.params.name;
-		if (serviceName === 'openbrewerydb') {
-			let type = reqEvent.params.type;
-			type = (type === 'list') ? '' : `/${type}`;
-			let path: any = reqEvent.params.path ? reqEvent.params.path : '';
-			console.log('server.GET.path:', path);
-			path = path ? path.split('/') : [];
-			console.log('server.GET.path:', path);
-			path = loChunk(path, 2);
-			console.log('server.GET.path:', path);
-			loForEach(path, function (value) {
-				search = search ? search + `&${value[0]}=${value[1]}` : search + `?${value[0]}=${value[1]}`;
-			});
-			console.log('server.GET.params.search:', search);
-
-			url = new URL(`https://api.openbrewerydb.org/v1/breweries${type}${search}`);
-
-			if (url && true) console.log('server.GET.url:', url.toString());
-
-			result = await getFakeData(url);
-		}
+		result = await fakeDataService(reqEvent.params);
 		return new Response(result);
 
 	} catch (ex: any) {
 		throw new Error(ex.message);
 	}
-
-
 };
