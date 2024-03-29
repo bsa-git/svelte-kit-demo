79e61476b9b00b28405745b498d574c8643dc687
Sergey Beskorovainy <bs261257@gmail.com>
2024-03-29 11:07:15 +0000

    Update

--- src/routes/api/calc-form_actions/+page.server.ts
+++ src/routes/api/calc-form_actions/+page.server.ts
@@ -1,4 +1,4 @@
-import { fail, redirect } from '@sveltejs/kit';
+import { fail, error, redirect } from '@sveltejs/kit';
 import type { Actions } from './$types';
 
 export const prerender = false;
--- src/routes/blog/[slug]/+error.svelte
+++ src/routes/blog/[slug]/+error.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
   import { page } from "$app/stores";
 
-  if($page.error) console.log('Page:',  $page)
+  if($page.error) console.log('PageError:',  $page)
 </script>
 
 <!-- <h1>{$page.status}: {$page.error? $page.error.message}</h1> -->
-<h1>Page Status: {$page.status}; Page Error: "{$page.error?.message}" </h1>
+<h1>Page Status: {$page.status}; PageError: "{$page.error?.message}" </h1>
